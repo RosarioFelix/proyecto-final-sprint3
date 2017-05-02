a2d855aac4ef62d6ceacbb027c33336b4ad66b4f
Rosario Felix <elijan_11@hotmail.com>
2017-05-02 17:59:55 -0500

    subiendo master

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,6 +3,12 @@ var lastScrollTop = 0;
 var enlace=document.getElementsByClassName('enlace');
 var boton=document.getElementById("lyft");
 var otroBoton=document.getElementById("otro-boton");
+var containerLogo=document.getElementById("conten-logo");
+var logo= document.getElementById("logo");
+
+var imagen= document.createElement("img");
+imagen.classList.add("box-img");
+imagen.setAttribute("src","assets/img/logo-pink.png");
 
 window.addEventListener("scroll", function(){
    var currentScroll = window.pageYOffset || document.documentElement.scrollTop;
@@ -10,6 +16,12 @@ window.addEventListener("scroll", function(){
      document.getElementById('nav-background').style.display="block";
      document.getElementById("nav-header").classList.add("header-in");
      document.getElementById("nav-header").style.opacity = 1;
+     imagen.style.display="block";
+     imagen.style.width="50px";
+     containerLogo.appendChild(imagen);
+
+
+     logo.style.display="none"; //quitando logo
      enlace[0].style.color="#000"
      enlace[1].style.color="#000"
      enlace[2].style.color="#000"
@@ -23,7 +35,8 @@ window.addEventListener("scroll", function(){
    if(currentScroll<=3){
        document.getElementById("nav-header").classList.remove("solid");
        document.getElementById("nav-header").classList.remove("header-in");
-
+       logo.style.display="block";
+       imagen.style.display="none";
 
         enlace[0].style.color="#fff"
         enlace[1].style.color="#fff"
